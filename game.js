ec4e5ad3f15c0dc4483eb78d07077c29da2e15b1
Ujjwal Verma <vujjwal1n@gmail.com>
2022-06-19 12:58:18 +0530

    Added animations and sounds

--- game.js
+++ game.js
@@ -5,6 +5,32 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
-//   console.log(randomChosenColor);
+  $("#"+randomChosenColor).fadeOut(100).fadeIn(100);
+  makeSound(randomChosenColor);
 }
-nextSequence();
+
+function makeSound(color){
+    switch(color){
+        case "green":
+            var greenAudio = new Audio("./sounds/green.mp3");
+            greenAudio.play();
+            break;
+        case "red":
+            var redAudio = new Audio("./sounds/red.mp3");
+            redAudio.play();
+            break;
+        case "blue":
+            var blueAudio = new Audio("./sounds/blue.mp3");
+            blueAudio.play();
+            break;
+        case "yellow":
+            var yellowAudio = new Audio("./sounds/yellow.mp3");
+            yellowAudio.play();
+            break;
+        default:
+            console.log(color);
+            break;
+    }
+}
+
+// nextSequence();
